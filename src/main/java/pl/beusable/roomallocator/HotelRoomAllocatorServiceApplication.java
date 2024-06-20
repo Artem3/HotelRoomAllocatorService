10382589a6b90735208b0ee767a90bbaecdcde3f
Artem Goncharenko <artem.goncharenko@gmail.com>
2024-06-20 13:36:29 +0200

    init commit

---
+++ src/main/java/pl/beusable/roomallocator/HotelRoomAllocatorServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.beusable.roomallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomAllocatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomAllocatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/beusable/roomallocator/HotelRoomAllocatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beusable.roomallocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomAllocatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
